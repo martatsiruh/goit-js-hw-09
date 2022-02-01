ab1eb569a7da2ada2fd4be4f84cce417cb97b88b
Marta Tsiruh <martatsiruch@gmail.com>
2022-02-01 22:43:35 +0100

    add Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,48 @@
+import '../css/common.css';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyEl = document.querySelector('body');
+const startBtnEl = document.querySelector("[data-start]");
+console.log(startBtnEl);
+const stopBtnEl = document.querySelector("[data-stop]");
+console.log(stopBtnEl);
+
+startBtnEl.addEventListener('click', onStartBtnClick);
+stopBtnEl.addEventListener('click', onStopBtnClick);
+
+let timerId = null;
+
+
+function onStartBtnClick() {
+    activeButtonClick();
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    //console.log(timerId)
+    
+}
+
+function onStopBtnClick() {
+    //disActiveButtonClick();
+    clearInterval(timerId);
+    //console.log(timerId);
+}
+
+function activeButtonClick(event) {
+    if (!event){
+    startBtnEl.disabled = true;
+        }else{
+    stopBtnEl.disabled = false;
+    }
+}
+
+function disActiveButtonClick(event) {
+    if (!event){
+    startBtnEl.disabled = true;
+        }else{
+    stopBtnEl.disabled = false;
+    }
+}
